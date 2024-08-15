c9d5235c26b532bbd3925707d33b0cb9be00135e
Pierre-Olivier Latour <pol@mac.com>
2024-08-15 11:50:03 +0200

    Update

--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -1,4 +1,8 @@
-import type { PlatformAccessory, Service } from 'homebridge';
+import type {
+  CharacteristicValue,
+  PlatformAccessory,
+  Service,
+} from 'homebridge';
 import { Socket } from 'net';
 import Modbus from 'jsmodbus';
 import PromiseSocket from 'promise-socket';
@@ -6,9 +10,11 @@ import PromiseSocket from 'promise-socket';
 import type { ExampleHomebridgePlatform } from './platform.js';
 
 const MODBUS_PORT = 502;
+const MODE_REGISTER = 0;
+const MODE_OFF = 0;
+const MODE_COMFORT_1 = 1;
 const SUPPLY_TEMPERATURE_REGISTER = 2005; // x10
 const EXTRACT_TEMPERATURE_REGISTER = 2006; // x10
-// 0 = mode (OFF = 0, Comfort 1 = 1, etc...)
 // 999-1008: alarms?
 // 2002 = supply flow (m3/h)
 // 2005 = extract flow (m3/h)
@@ -19,6 +25,7 @@ const EXTRACT_TEMPERATURE_REGISTER = 2006; // x10
 // 2220 = total recovered energy (kWh)
 
 export class ExamplePlatformAccessory {
+  private activeSwitch: Service;
   private supplyTemperature: Service;
   private extractTemperature: Service;
 
@@ -31,6 +38,18 @@ export class ExamplePlatformAccessory {
       .setCharacteristic(this.platform.Characteristic.Manufacturer, 'Komfovent')
       .setCharacteristic(this.platform.Characteristic.Model, 'N/A')
       .setCharacteristic(this.platform.Characteristic.SerialNumber, 'N/A');
+
+    this.activeSwitch =
+      this.accessory.getService('Active') ||
+      this.accessory.addService(
+        this.platform.Service.Switch,
+        'Active',
+        'active',
+      );
+    this.activeSwitch
+      .getCharacteristic(this.platform.Characteristic.On)
+      .onGet(this.getActiveSwitch.bind(this))
+      .onSet(this.setActiveSwitch.bind(this));
 
     this.supplyTemperature =
       this.accessory.getService('Supply') ||
@@ -55,7 +74,7 @@ export class ExamplePlatformAccessory {
       .onGet(this.getExtractTemperature.bind(this));
   }
 
-  async readRegister(register: number) {
+  async readRegister(register: number): Promise<number> {
     const socket = new Socket();
     const client = new Modbus.client.TCP(socket);
     const promiseSocket = new PromiseSocket(socket);
@@ -83,6 +102,29 @@ export class ExamplePlatformAccessory {
     return result;
   }
 
+  async writeRegister(register: number, value: number): Promise<void> {
+    const socket = new Socket();
+    const client = new Modbus.client.TCP(socket);
+    const promiseSocket = new PromiseSocket(socket);
+
+    try {
+      await promiseSocket
+        .setTimeout(3000)
+        .connect(MODBUS_PORT, this.platform.config.ip_address);
+      await client.writeSingleRegister(register, value);
+    } catch (error) {
+      if (error instanceof Error) {
+        this.platform.log.error(error.name, error.message);
+      } else if (typeof error === 'string') {
+        this.platform.log.error(error);
+      } else {
+        this.platform.log.error('Unknown exception', typeof error);
+      }
+    } finally {
+      await promiseSocket.end();
+    }
+  }
+
   async getSupplyTemperature(): Promise<number> {
     this.platform.log.debug('Triggered getSupplyTemperature');
     const value = await this.readRegister(SUPPLY_TEMPERATURE_REGISTER);
@@ -93,5 +135,17 @@ export class ExamplePlatformAccessory {
     this.platform.log.debug('Triggered getExtractTemperature');
     const value = await this.readRegister(EXTRACT_TEMPERATURE_REGISTER);
     return value / 10;
+  }
+
+  async getActiveSwitch(): Promise<boolean> {
+    this.platform.log.debug('Triggered getActiveSwitch');
+    const value = await this.readRegister(MODE_REGISTER);
+    return value !== MODE_OFF;
+  }
+
+  async setActiveSwitch(value: CharacteristicValue): Promise<void> {
+    const active = value as boolean;
+    this.platform.log.debug('Triggered setActiveSwitch');
+    this.writeRegister(MODE_REGISTER, active ? MODE_COMFORT_1 : MODE_OFF);
   }
 }
